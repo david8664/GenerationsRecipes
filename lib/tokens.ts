967d7ed556c027266bde6f31edde3bc1217bdd0e
David <computerdavid494@gmail.com>
2024-05-19 18:24:25 +0300

    Fix: remove data folder

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,15 +1,14 @@
 import { v4 as uuidv4 } from "uuid";
 import crypto from "crypto";
 import { db } from "@/lib/db";
-import { getVerificationTokenByEmail } from "@/data/verification-token";
-import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
-import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 
 const generateVerificationToken = async (email: string) => {
   const token = uuidv4();
   const expires = new Date(new Date().getTime() + 3600 * 1000);
 
-  const existingToken = await getVerificationTokenByEmail(email);
+  const existingToken = await db.verificationToken.findFirst({
+    where: { token: email },
+  });
   if (existingToken) {
     await db.verificationToken.delete({ where: { id: existingToken.id } });
   }
@@ -25,7 +24,9 @@ const generatePasswordResetToken = async (email: string) => {
   const token = uuidv4();
   const expires = new Date(new Date().getTime() + 3600 * 1000);
 
-  const existingToken = await getPasswordResetTokenByEmail(email);
+  const existingToken = await db.passwordResetToken.findFirst({
+    where: { email },
+  });
   if (existingToken) {
     await db.passwordResetToken.delete({ where: { id: existingToken.id } });
   }
@@ -41,7 +42,7 @@ const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString();
   const expires = new Date(new Date().getTime() + 10 * 60 * 1000);
 
-  const existingToken = await getTwoFactorTokenByEmail(email);
+  const existingToken = await db.twoFactorToken.findFirst({ where: { email } });
   if (existingToken) {
     await db.twoFactorToken.delete({ where: { id: existingToken.id } });
   }
